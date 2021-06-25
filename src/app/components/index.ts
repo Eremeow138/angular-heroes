ffbd1af0f0486f08f0dce8d090aff723fe9933a4
Eremeow138 <eremeow138@yandex.ru>
2021-06-25 21:41:33 +0800

    feat: build and prepare for gh-pages

--- src/app/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { HeroSearchComponent } from './hero-search/hero-search.component';
-
-export { DashboardComponent } from './dashboard/dashboard.component';
-
-export { MessagesComponent } from './messages/messages.component';
-
-export { HeroesComponent } from './heroes/heroes.component';
-export { HeroDetailComponent } from './hero-detail/hero-detail.component';
--- src/app/services/index.ts
+++
@@ -1,5 +0,0 @@
-export { InMemoryDataService } from './in-memory-data.service';
-
-export { MessageService } from './messages.service';
-
-export { HeroService } from './hero.service';
