5b40ee1703c982e297f38e29629bcff532ee3073
Eremeow138 <eremeow138@yandex.ru>
2021-06-25 10:40:18 +0800

    feat: implement the hero editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeroesComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
